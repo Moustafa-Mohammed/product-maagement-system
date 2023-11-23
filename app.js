1cd688b4617d6a6e95183ee9e2d376b1bb26a7f2
Mostafa Mohamed <thedevelopermostafa@gmail.com>
2023-11-23 08:42:07 +0200

    Completed the inputs section, finished the add product, update product, and delete product functionalities, saved the data to local storage

---
+++ app.js
@@ -0,0 +1,112 @@
+const title = document.querySelector("#title");
+const category = document.querySelector("#category");
+const price = document.querySelector("#price");
+const status = document.querySelector("#status");
+const taxes = document.querySelector("#taxes");
+const discount = document.querySelector("#discount");
+const addProductBtn = document.querySelector("#addProduct");
+const updateProductBtn = document.querySelector("#updateProduct");
+const deleteProductBtn = document.querySelector("#deleteProduct");
+const total = document.querySelector("#total");
+const productsTable = document.querySelector("tbody");
+
+let currentProductIndex;
+
+// initialize the data
+const products = JSON.parse(localStorage.getItem("products")) || [];
+
+// get total amount
+function getTotalAmount() {
+  let totalAmount = +price.value + +taxes.value + -+discount.value;
+  total.innerHTML = totalAmount;
+}
+
+const amounts = Array.from(document.querySelector(".amounts").children);
+
+amounts.forEach((amount) => {
+  amount.addEventListener("input", getTotalAmount);
+});
+
+// Add product
+function addProduct() {
+  const product = {
+    title: title.value,
+    category: category.value,
+    taxes: +taxes.value,
+    discount: +discount.value,
+    price: +price.value,
+    status: status.value,
+  };
+  if (addProductBtn.innerHTML === "Add Product") {
+    products.push(product);
+  } else {
+    products[currentProductIndex] = product;
+  }
+  addProductBtn.innerHTML === "Add Product";
+  localStorage.setItem("products", JSON.stringify(products));
+
+  clearInputs();
+  displayProducts();
+}
+
+// clear inputs
+function clearInputs() {
+  title.value = "";
+  category.value = "";
+  taxes.value = "";
+  discount.value = "";
+  price.value = "";
+  status.value = "";
+}
+
+addProductBtn.onclick = (e) => {
+  e.preventDefault();
+  addProduct();
+};
+
+function displayProducts() {
+  productsTable.innerHTML = "";
+  products.forEach((p, index) => {
+    const productTemplate = `<tr>
+    <td>${index}</td>
+    <td>${p.title}</td>
+    <td>${p.price}</td>
+    <td>${p.taxes}</td>
+    <td>${p.status}</td>
+    <td>${p.discount}</td>
+    <td>100</td>
+    <td>${p.category}</td>
+    <td><button onclick="updateProduct(${index})">Update</button></td>
+    <td><button class="delete" onclick="deleteProduct(${index})">delete</button></td>
+  </tr>`;
+
+    productsTable.insertAdjacentHTML("beforeend", productTemplate);
+  });
+}
+
+// remove product
+function deleteProduct(index) {
+  products.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(products));
+  displayProducts();
+}
+
+// update product
+function updateProduct(index) {
+  addProductBtn.innerHTML = "Update Product";
+  const product = products.find((p, i) => i === index);
+  currentProductIndex = index;
+  title.value = product.title;
+  price.value = product.price;
+  category.value = product.category;
+  status.value = product.status;
+  discount.value = product.discount;
+  taxes.value = product.taxes;
+  getTotalAmount();
+}
+
+window.onload = () => {
+  getTotalAmount();
+  displayProducts();
+  clearInputs();
+};
